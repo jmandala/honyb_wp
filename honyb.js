9c7f0799432bca2b53c5497adcda2e88e1864f75
Joshua Jacobs <josh@mandala-designs.com>
2012-01-24 10:32:51 -0500

    Short code will link to specific sku on honyb.com and display a custom link value
    - honyb.js: link to specific sku
    - honyb.php: enqueue honyb.css; make honyb_tag short code include caption/title for link

--- honyb.js
+++ honyb.js
@@ -1,3 +1,13 @@
 jQuery(document).ready(function() {
-	jQuery('.honyb-section').colorbox({iframe: true, width: '80%', height: '80%', href: 'http://www.honyb.com'});
+    jQuery('.honyb-popup').each(function () {
+        var popup = jQuery(this);
+        var honyb_url = 'http://www.honyb.com/embed/products/';
+        if (popup.data('sku')) {
+            honyb_url =  honyb_url + popup.data('sku');
+            jQuery(this).colorbox({iframe:true, width:'80%', height:'80%', href:honyb_url});
+        }
 });
+});
+
+
+
--- honyb.php
+++ honyb.php
@@ -3,7 +3,7 @@
 Plugin Name: HonyB Integration
 Plugin URI: http://www.honyb.com/integration
 Description: Provides HonyB Books to WordPress sites
-Version: 0.1
+Version: 0.2
 Author: Joshua Jacobs
 Author URI: http://joshuajacobs.name
 License: GPL2
@@ -15,18 +15,24 @@ License: GPL2
 function load_javascript() {
 	wp_enqueue_script('colorbox.js', plugins_url('/colorbox/colorbox/jquery.colorbox-min.js', __FILE__), array('jquery'), '1.0');
 	wp_enqueue_style('colorbox.css', plugins_url('/colorbox/example1/colorbox.css', __FILE__));
+    wp_enqueue_style('honyb.css', plugins_url('/honyb.css', __FILE__));
 	wp_enqueue_script('honyb', plugins_url('/honyb.js', __FILE__), array('jquery'), '1.0');
 }
 
 add_action('wp_enqueue_scripts', 'load_javascript');
 
+/**
+ * [honyb sku="9781234567890" title="The Are of War"]
+ * @param $atts Array the attributes provided: sku, title
+ * @return string
+ */
+function honyb_tag($atts)
+{
+    extract(shortcode_atts(array('sku' => '', 'title' => ''), $atts));
 
-// [honyb sku="9781234567890"]
-function honyb_tag( $atts ) {
-	extract(shortcode_atts( array('sku' => ''), $atts));
-
-	//return "<div class='honyb-section'>Buy from Honyb <span clss='sku'>{$sku}</span></div>";
-	return "<div class='honyb-popup'><a href='http://www.honyb.com/' target='_blank'>Buy from Honyb <span clss='sku'>{$sku}</span></a></div>";
+    /** @var $sku String */
+    /** @var $title String */
+    return "<div class='honyb'><div class='honyb-popup button orange' data-sku='{$sku}'>Buy {$title}!</div><div class='info'>Buying direct supports this website!</div></div>";
 }
 
 add_shortcode('honyb', 'honyb_tag');
