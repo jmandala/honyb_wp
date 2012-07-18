6a0e1a20975221e7532e1fca289e7d2896c30bbc
Joshua Jacobs <josh@mandala-designs.com>
2012-07-18 16:21:11 -0400

    beautify

--- honyb.js
+++ honyb.js
@@ -1,20 +1,17 @@
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
     jQuery('.honyb-popup').each(function () {
         var popup = jQuery(this);
         var honyb_url = 'http://www.honyb.com/embed/products/';
         if (popup.data('sku')) {
-            honyb_url =  honyb_url + popup.data('sku');
+            honyb_url = honyb_url + popup.data('sku');
             jQuery(this).colorbox({
                 iframe:true,
                 width:'650px',
                 height:'80%',
                 href:honyb_url,
-                opacity: .7,
-                overlayClose: false
+                opacity:.7,
+                overlayClose:false
             });
         }
+    });
 });
-});
-
-
-
